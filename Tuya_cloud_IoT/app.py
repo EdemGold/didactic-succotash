49ad115a88c50166fa34acbb89606988b764bb40
EdemGold <ekmedm@gmail.com>
2022-02-11 16:32:48 -0800

    first commit

---
+++ Tuya_cloud_IoT/app.py
@@ -0,0 +1,9 @@
+ACCESS_ID = 'tkcj73hpaxagrps4jzta'
+
+ACCESS_KEY = 'ca5055d7ba1644b1b644ff92f55bbff9'
+
+ENDPOINT = "https://openapi.tuyacn.com"
+
+USERNAME = 'ekmedm@gmail.com'
+
+PASSWORD = 'pass2406'
